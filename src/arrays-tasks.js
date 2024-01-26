10b9bd653c10e95af1873e06c7b9c5d3b7c6b374
karl <egor.perfilje2016v@yandex.ru>
2024-01-26 09:14:27 +0300

    feat: implement isSameLength function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -147,8 +147,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const elementLength = arr[0].length;
+  return arr.every((elem) => elem.length === elementLength);
 }
 
 /**
