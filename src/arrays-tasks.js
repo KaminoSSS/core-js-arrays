38d6d3d331d710c3bdbb572908f90d4312ddb013
karl <egor.perfilje2016v@yandex.ru>
2024-01-26 08:01:38 +0300

    feat: implement sumArrays function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,13 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const theLongestArr = arr1.length >= arr2.length ? arr1 : arr2;
+  const theShortestArr = arr1.length < arr2.length ? arr1 : arr2;
+
+  return theLongestArr.map(
+    (value, index) => value + (theShortestArr[index] || 0)
+  );
 }
 
 /**
