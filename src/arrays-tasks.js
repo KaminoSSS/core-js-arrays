769bbf2166500ce581ba3259a8c777292f3eb4f7
karl <egor.perfilje2016v@yandex.ru>
2024-01-26 09:07:13 +0300

    feat: implement getAverage function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -124,8 +124,17 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  return arr.length === 0
+    ? 0
+    : Math.round(
+        (arr.reduce(
+          (accumulator, currentValue) => accumulator + currentValue,
+          0
+        ) /
+          arr.length) *
+          100
+      ) / 100;
 }
 
 /**
