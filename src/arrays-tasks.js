bcf601e7aa3855ce8fb80cda968822afebeaa0f8
Egor Perfiliev <perfilev.egor2016@gmail.com>
2024-02-10 15:21:54 +0300

    feat: implement createNDimensionalArray function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -175,7 +175,7 @@ function isValueEqualsIndex(/* arr */) {
  * @param {number} index - Specified index.
  *
  * @example
- *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
+ *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4,  5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
 function insertItem(arr, item, index) {
@@ -272,8 +272,12 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  if (n <= 1) {
+    return new Array(size).fill(0);
+  }
+
+  return new Array(size).fill(createNDimensionalArray(n - 1, size));
 }
 
 /**
@@ -337,8 +341,16 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const resultArr = arr.reduce((acc, item, index) => {
+    const chunkIndex = Math.floor(index / chunkSize);
+    if (!Array.isArray(acc[chunkIndex])) {
+      acc[chunkIndex] = [];
+    }
+    acc[chunkIndex].push(item);
+    return acc;
+  }, []);
+  return resultArr;
 }
 
 /**
